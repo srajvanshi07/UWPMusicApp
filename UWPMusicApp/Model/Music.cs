bc1e701a8ae77e29e792ca7ca10fa23b4d916506
rajvanshishweta <42074002+rajvanshishweta@users.noreply.github.com>
2020-02-17 11:20:07 -0800

    deply my repo in github.com

---
+++ UWPMusicApp/Model/Music.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPMusicApp.Model
+{
+    public class Music
+    {
+        // public string Pop { get; set; }
+        //public string Jazz { get; set; }
+        // public string Rock { get; set; }
+        //public string HipHop { get; set; }
+        //public string  Classical { get; set; }
+        //public string International { get; set; }
+        public string Name { get; set; }
+        public SoundCategory Category { get; set; }
+        public string AudioFile { get; set; }
+        public string Imagefile { get; set; }
+
+    }
+}
